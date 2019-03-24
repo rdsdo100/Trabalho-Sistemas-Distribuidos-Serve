bb1292487a2cd7d91271a93e92c2d425d5ae321f
Rubens <rdsdo2011@gmail.com>
2019-03-24 02:41:42 -0300

    Quase concluido o basico

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,14 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Server {
+
+    List<PrintWriter> escritores = new ArrayList<>();
 
     Scanner scanner;
 
@@ -14,11 +19,22 @@ public class Server {
             while (true) {
                 Socket socket = serverSocket.accept();
                 new Thread(new EscutaCliente(socket)).start();
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                escritores.add(printWriter);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void encaminharParaTodos(String texto){
+        for (PrintWriter printWriter : escritores){
+            try{
+                printWriter.println(texto);
+                printWriter.flush();
+            }catch (Exception e){}
+        }
     }
 
     private class EscutaCliente implements Runnable {
@@ -39,6 +55,8 @@ public class Server {
                 String texto;
                 while ((texto = scanner.nextLine()) != null) {
                     System.out.println("Rcecebeu: " + texto);
+                    encaminharParaTodos(texto);
+
                 }
             } catch (Exception e) {
             }
